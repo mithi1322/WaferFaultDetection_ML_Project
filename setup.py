6c0e02021985c4dbbe4cc464f07381aaf0a937ed
mithi1322 <mithileshjaiswal0000@gmail.com>
2023-08-26 18:52:55 +0530

    This is my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="WaferFaultDetection",
+    version="0.0.1",
+    author="Mithilesh Kumar Jaiswal",
+    author_email="mithileshjaiswal0000@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
---
+++ src/constant/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "ML_Project"
+MONGO_COLLECTION_NAME = "waferfault"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://mithileshjaiswal:mkj1234@cluster0.p1yajf0.mongodb.net/?retryWrites=true&w=majority"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
